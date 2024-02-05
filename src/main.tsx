54c39f3632a47a49134a0c416f76b22dd4356bec
Ritu Raj <alchemyrecloak@gmail.com>
2024-02-05 16:57:11 +0530

    gcp configuration changed - added test usrs

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
  
-  <GoogleOAuthProvider clientId="http://837209385029-qmvr5p9rkisprpc8ssoeqggqafnv5jd5.apps.googleusercontent.com/">
+  <GoogleOAuthProvider clientId="837209385029-qmvr5p9rkisprpc8ssoeqggqafnv5jd5">
    <App />
   </GoogleOAuthProvider>
 
