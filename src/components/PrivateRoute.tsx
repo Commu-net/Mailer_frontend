d72aea9f6f7a002b786c1c753480d47c6e38879c
Ritu Raj <ritu.12797.raj@gmail.com>
2024-03-09 13:48:52 +0530

    testing from main repo

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -4,5 +4,5 @@ import { useSelector} from 'react-redux'
 
 export default function PrivateRoute({ children }: { children: React.ReactNode }) {
   const isLogggednIn = useSelector((state: any) => state.auth.isLogggednIn)
-  return (isLogggednIn ? children : (<Navigate to="/login" />))
+  return (isLogggednIn ? children : (<Navigate to="/login" />))  
 }
