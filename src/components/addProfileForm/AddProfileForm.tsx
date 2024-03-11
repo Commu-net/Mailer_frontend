efb6fec91088c43fe337378ed90ba039e207fd02
rituraj12797 <alchemyrecloak@gmail.com>
2024-03-11 18:25:55 +0530

    testing add profile function

--- src/components/addProfileForm/AddProfileForm.tsx
+++ src/components/addProfileForm/AddProfileForm.tsx
@@ -52,7 +52,7 @@ export async function addProfile(values: z.infer<typeof profileSchema>,userId:st
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            userId: userId,
+            userId: '65c3c7ab19728265ad970414',
             data: [{
                 email: values.email,
                 currentDesignation: values.designation,
