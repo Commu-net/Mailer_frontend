a275b821dd1917633a5e8352fde07e8de29e5e57
rituraj12797 <alchemyrecloak@gmail.com>
2024-03-11 23:12:05 +0530

    testing update profile

--- src/components/editProfileForm/EditProfileForm.tsx
+++ src/components/editProfileForm/EditProfileForm.tsx
@@ -86,7 +86,7 @@ export default function EditProfileForm(rowData:any) {
 
     // 2. Define a submit handler.
     function submithandler(values: z.infer<typeof profileSchema>,userid:string) {
-        console.log("these are the Updte form values",values,userid,rowData._id)
+        console.log("these are the Updte form values",values,userid,rowData)
         const rowId = rowData._id
         EditProfile(values,userid,rowId)
     }
