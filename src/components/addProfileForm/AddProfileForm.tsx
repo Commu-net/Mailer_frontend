cbcaadf84df4eef622273cdad3c80ceae514bb3f
rituraj12797 <alchemyrecloak@gmail.com>
2024-03-13 23:05:08 +0530

    added toast and auto close in AddProfile

--- src/components/addProfileForm/AddProfileForm.tsx
+++ src/components/addProfileForm/AddProfileForm.tsx
@@ -19,7 +19,10 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 
-import {RootState } from "@/redux/store"
+import { RootState } from "@/redux/store"
+
+import { useToast } from "@/components/ui/use-toast"
+import { DialogClose } from "@radix-ui/react-dialog"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -32,41 +35,56 @@ import { useForm } from "react-hook-form"
 import { useSelector } from "react-redux"
 
 const profileSchema = z.object({
-    name:z.string({required_error:"name is required"}).min(2,'name should not be empty or less than 2 letter').max(50),
-    email:z.string().email(),
-    company:z.string().min(2).max(50),
-    designation:z.string().min(2).max(50),
+    name: z.string({ required_error: "name is required" }).min(2, 'name should not be empty or less than 2 letter').max(50),
+    email: z.string().email(),
+    company: z.string().min(2).max(50),
+    designation: z.string().min(2).max(50),
 })
 
-export async function addProfile(values: z.infer<typeof profileSchema>,userId:string) {
-    
-    values.company = values.company? values.company : " - "
-    values.designation = values.designation? values.designation : " - "
-     
-    console.log(values,userId)
-   let data = await fetch("https://api.api-communet.tech/api/v1/mail", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            userId: userId,
-            data: [{
-                email: values.email,
-                currentDesignation: values.designation,
-                name: values.name,
-                company: values.company,
-            }],
-        }),
-    })
-    let response = await data.json()
-    console.log(response)
-    
+export async function addProfile(values: z.infer<typeof profileSchema>, userId: string, toast: Function) {
+
+    values.company = values.company ? values.company : " - "
+    values.designation = values.designation ? values.designation : " - "
+
+    console.log(values, userId)
+    try {
+        let data = await fetch("https://api.api-communet.tech/api/v1/mail", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                userId: userId,
+                data: [{
+                    email: values.email,
+                    currentDesignation: values.designation,
+                    name: values.name,
+                    company: values.company,
+                }],
+            }),
+        })
+        let response = await data.json()
+        console.log(response)
+        if (response.statusCode === 200) {
+            toast({
+                title: "Successfully added profile",
+                className: " text-green-600 bg-green-100"
+            })
+        }
+    } catch (error) {
+        toast({
+            title: "An error occured while saving this profile",
+            className: " text-red-600 bg-red-100"
+        })
+    }
+
 }
 
 export default function AddProfileForm() {
+    const { toast } = useToast()
+
     const userId = useSelector((state: RootState) => state.userData.userId)
-    console.log("this is user id ",userId)
+    console.log("this is user id ", userId)
     const form = useForm<z.infer<typeof profileSchema>>({
         resolver: zodResolver(profileSchema),
         defaultValues: {
@@ -79,15 +97,15 @@ export default function AddProfileForm() {
     })
 
     // 2. Define a submit handler.
-    function submitHandler(values: z.infer<typeof profileSchema>,userId:string) {
-        addProfile(values,userId)
+    function submitHandler(values: z.infer<typeof profileSchema>, userId: string, toast: Function) {
+        addProfile(values, userId, toast)
     }
 
     return (<Dialog>
         <DialogTrigger asChild>
-            <Button variant={"default"} className='h-[100%] w-[100px]  text-black rounded-sm border-dashed border-[1px] border-black flex justify-center items-center gap-[10px] bg-white active:bg-white'> 
-            <div><GoPlusCircle /></div>
-            <div>Add</div>
+            <Button variant={"default"} className='h-[100%] w-[100px]  text-black rounded-sm border-dashed border-[1px] border-black flex justify-center items-center gap-[10px] bg-white active:bg-white'>
+                <div><GoPlusCircle /></div>
+                <div>Add</div>
             </Button>
         </DialogTrigger>
         <DialogContent className="W-[300px] sm:max-w-[425px] h-[500px]">
@@ -97,7 +115,7 @@ export default function AddProfileForm() {
             <Form {...form}>
                 <form onSubmit={(event) => {
                     event.preventDefault()
-                    submitHandler(form.getValues(),userId)
+                    submitHandler(form.getValues(), userId, toast)
                     console.log('submitted')
                 }} className="space-y-4 flex flex-col ">
                     <FormField
@@ -119,7 +137,7 @@ export default function AddProfileForm() {
                         render={({ field }) => (
                             <FormItem>
                                 <FormLabel>Email</FormLabel>
-                                
+
 
                                 <FormControl>
                                     <Input
@@ -132,7 +150,7 @@ export default function AddProfileForm() {
                             </FormItem>
                         )}
                     />
-                   <FormField
+                    <FormField
                         control={form.control}
                         name="company"
                         render={({ field }) => (
@@ -159,7 +177,9 @@ export default function AddProfileForm() {
                         )}
                     />
                     <div className="w-[100%] flex justify-center items-center">
-                    <Button type="submit" className="w-[20%] ">Create</Button>
+                        <DialogClose asChild>
+                            <Button type="submit" className="w-[20%] ">Create</Button>
+                        </DialogClose>
 
                     </div>
                 </form>
