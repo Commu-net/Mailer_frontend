d9e1a082fdfb8f9988265c753756917c80433a6e
rituraj12797 <alchemyrecloak@gmail.com>
2026-05-16 12:03:45 +0530

    made some changes here and there

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
         <div className='footerbottom h-3/4 w-4/5 text-[min(3.5vw,1.2rem)] flex justify-around text-[rgba(255,255,255,0.779)] flex-col lg:flex-row lg:'>
         <div className='footer_desc h-full w-7/10 flex justify-center items-center'>Excited to use but don't know where and how to start how to use ? head to our resources page.</div>
         <div className='link h-[100%] w-[100%] flex items-center justify-center  lg:w-[30%]'>
-            <Link to="/resources" className='gs_link h-1/2 rounded-md flex items-center justify-center w-[min(300px,80%)] bg-white lg:h-1/3'>Get started</Link>
+            <Link to="/resources" className='gs_link h-1/2 rounded-md flex items-center justify-center w-[min(300px,80%)] bg-white lg:h-1/3'>Get Used to it</Link>
         </div>
         </div>
     </div> );
